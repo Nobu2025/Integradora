d477df378c96973a9c796f4771e23cf96a40e080
Nobu2025 <20243rd038@utez.edu.mx>
2025-07-03 01:59:59 -0600

    se agrego la funcion sumar

--- src/main/java/mx/edu/utez/vista/HelloController.java
+++ src/main/java/mx/edu/utez/vista/HelloController.java
@@ -2,6 +2,14 @@ package mx.edu.utez.vista;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -24,7 +32,14 @@ public class HelloController {
     }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected  void SumaV(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("suma.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+
+
     }
 }
+
